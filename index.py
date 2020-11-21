332d5f95fa3a2da69577c7c0b8273303d9eafe16
RupiR <rupalirajput@gmail.com>
2020-11-21 12:35:30 -0600

    python syntax numbers

--- index.py
+++ index.py
@@ -21,4 +21,15 @@ print(meal)
 
 
 
+# ***** Python - Numbers *****
+# Define the release and runtime integer variables below:
+release_year = 1996
+runtime = 5
+
+
+# Define the rating_out_of_10 float variable below: 
+rating_out_of_10 = 2.5
+
+
+
 
