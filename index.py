f4a5d0f937bc139b942f9aef1de6786f3ded98b2
RupiR <rupalirajput@gmail.com>
2020-11-21 12:49:08 -0600

    exponents

--- index.py
+++ index.py
@@ -21,7 +21,8 @@ print(meal)
 
 
 
-# ***** Python - Numbers *****
+# ***** Python *****
+# Numbers
 # Define the release and runtime integer variables below:
 release_year = 1996
 runtime = 5
@@ -30,7 +31,7 @@ runtime = 5
 # Define the rating_out_of_10 float variable below: 
 rating_out_of_10 = 2.5
 
-
+# changing numbers
 quilt_width = 8
 quilt_length = 12
 
@@ -39,5 +40,19 @@ print(quilt_width * quilt_length)
 quilt_length = 8
 
 print(quilt_width * quilt_length)
+
+# exponents
+# Calculation of squares for:
+# 6x6 quilt
+print(6 ** 2)
+
+# 7x7 quilt
+print(7 ** 2)
+
+# 8x8 quilt
+print(8 ** 2)
+
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+print(6 * 6 * 6 * 6)
 
 
