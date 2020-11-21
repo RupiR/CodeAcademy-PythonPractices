ed5b97d136a672446cb124dcd4e4f2e1c937bcd9
RupiR <rupalirajput@gmail.com>
2020-11-21 12:59:25 -0600

    modulo

--- index.py
+++ index.py
@@ -56,3 +56,19 @@ print(8 ** 2)
 print(6 * 6 * 6 * 6)
 
 
+# modulo
+#1. You’re trying to divide a group into four teams. All of you count off, and you get number 27. Find out your team by computing 27 modulo 4. Save the value to my_team.
+my_team = 27 % 4
+
+#2. Print out my_team. What number team are you on?
+print(my_team)
+
+#3. Food for thought: what number team are the two people next to you (26 and 28) on? What are the numbers for all 4 teams? (Optional Challenge Question)
+my_team = 26 % 4
+
+print(my_team)
+
+my_team = 28 % 4
+
+print(my_team)
+
