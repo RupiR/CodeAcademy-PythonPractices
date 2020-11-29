2bbac713ad8d696c213fefc44c80aac4086913ad
RupiR <rupalirajput@gmail.com>
2020-11-29 13:18:31 -0600

    write a function

--- index.py
+++ index.py
@@ -115,3 +115,18 @@ And why should I not speak to you?"""
 
 
 print(to_you)
+
+
+
+# what is a function
+def sing_song():
+      print("You may say I'm a dreamer")
+  print("But I'm not the only one")
+  print("I hope some day you'll join us")
+  print("And the world will be as one")
+  
+# call sing_song() below:
+sing_song()
+sing_song()
+
+
