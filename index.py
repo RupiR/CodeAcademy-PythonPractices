aed17fe0a31718893e4f635242d6af4dac6b5e6b
RupiR <rupalirajput@gmail.com>
2020-11-29 13:46:33 -0600

    keyword arguments

--- index.py
+++ index.py
@@ -146,3 +146,16 @@ def about_this_computer():
 print("This is your desktop")
 
 about_this_computer()
+
+
+
+# keyword arguments
+
+# Define create_spreadsheet():
+def create_spreadsheet(title, row_count = 1000):
+  print("Creating a spreadsheet called "+title+"with" +str(row_count)+"rows." )
+
+# Call create_spreadsheet() below with the required arguments:
+create_spreadsheet("Downloads")
+create_spreadsheet("Applications", 10)
+
