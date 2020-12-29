424a8240d10f1ec9685c468b0f2aba7531f73fef
RupiR <rupalirajput@gmail.com>
2020-12-29 11:56:49 -0500

    square root of a num

--- index.py
+++ index.py
@@ -241,6 +241,7 @@ print("The GE train does "+str(train_work)+" Joules of work over "+str(train_dis
 
 
 #function to power
+
 # Write your tenth_power function here:
 def tenth_power(num):
   return num**10
@@ -251,3 +252,15 @@ print(tenth_power(0))
 # 0 to the 10th power is 0
 print(tenth_power(2))
 # 2 to the 10th power is 1024
+
+
+# function square root
+# Write your square_root function here:
+def square_root(num):
+  result = num ** 0.5
+  return result
+# Uncomment these function calls to test your square_root function:
+print(square_root(16))
+# should print 4
+print(square_root(100))
+# should print 10
