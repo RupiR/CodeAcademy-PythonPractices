0fefd8168405940dc9fb07c39c3f1e63b34139d2
RupiR <rupalirajput@gmail.com>
2020-12-29 12:15:17 -0500

    remainder function modulo operator

--- index.py
+++ index.py
@@ -296,3 +296,16 @@ print(average(1, -1))
 
 
 
+# Remainder function - the python modulo operator
+
+# Write your remainder function here:
+def remainder(num1, num2):
+  return (num1 * 2) % (num2 / 2)
+# Uncomment these function calls to test your remainder function:
+print(remainder(15, 14))
+# should print 2
+print(remainder(9, 6))
+# should print 0
+
+
+
