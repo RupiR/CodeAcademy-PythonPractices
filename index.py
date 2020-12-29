e2b3c4fe821b34f0c1bb7d192e78ddd929f4d720
RupiR <rupalirajput@gmail.com>
2020-12-29 12:05:25 -0500

    average function

--- index.py
+++ index.py
@@ -278,3 +278,21 @@ print(win_percentage(5, 5))
 # should print 50
 print(win_percentage(10, 0))
 # should print 100
+
+
+
+
+# Average function
+
+# Write your average function here:
+def average(num1, num2):
+  return (num1 + num2) / 2
+# Uncomment these function calls to test your average function:
+print(average(1, 100))
+# The average of 1 and 100 is 50.5
+print(average(1, -1))
+# The average of 1 and -1 is 0
+
+
+
+
